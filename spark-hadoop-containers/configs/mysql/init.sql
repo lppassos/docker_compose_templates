a51b8de16ddd912d85be83eb9b3faa51b42c6b83
Luis Passos <luis.passos@datacentric-tech.com>
2024-09-03 23:11:06 +0100

    Add standalone metastore to the environment

--- spark-hadoop-containers/configs/mysql/init.sql
+++ spark-hadoop-containers/configs/mysql/init.sql
@@ -1,1 +1,5 @@
 CREATE DATABASE IF NOT EXISTS hue;
+CREATE DATABASE IF NOT EXISTS metastore;
+drop user if exists hive;
+CREATE USER hive IDENTIFIED BY 'hivepassword';
+GRANT ALL PRIVILEGES ON metastore.* TO hive IDENTIFIED BY 'hivepassword';
