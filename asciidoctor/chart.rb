923f5b0c52433d04fdd606fa5f3fc8c2dcbd7347
Luis Passos <lpcarvalho2@gmail.com>
2025-09-02 19:52:18 +0100

    Reorganize the code and start including rdoc

--- asciidoctor/chart.rb
+++ asciidoctor/chart.rb
@@ -8,6 +8,11 @@ Asciidoctor::Extensions.register do
   block ChartBlockProcessor, :chart
 end
 
+# Large chart image width to ensure when scaling in the pdf it works well
+CHART_SCALABLE_WIDTH = 2200
+
+# All the information we need for drawing the chart, extracted from the
+# contents of the block
 class ChartMetadata
   attr_accessor :type, :title, :xFields, :yFields, :yNames, :data
 
@@ -17,6 +22,44 @@ class ChartMetadata
     @yFields = []
     @yNames = []
     @data = []
+  end
+
+  # Loads the definition from the block reader. The data shows up after the
+  # "data:" line in a csv format. The csv starts with a header line specifying
+  # the names of the columns in the data set
+  #
+  # @param reader the reader passed to the BlockProcessor
+  def parse_content(reader)
+    in_data_mode = false
+    first_row = true
+    header = []
+    reader.lines.each do |line|
+      if in_data_mode
+        row_data = line.strip.split(",").map(&:strip)
+        if first_row
+          header = row_data.clone
+          first_row = false
+        else
+          @data << header.zip(row_data).to_h
+        end
+      else
+        key, value = line.strip.split(":").map(&:strip)
+        case key
+        when "data"
+          in_data_mode = true
+        when "type"
+          @type = value
+        when "title"
+          @title = value
+        when "x-fields"
+          @xFields = value
+        when "y-fields"
+          @yFields = value.split(',').map(&:strip)
+        else
+          raise "Unknown chart setting #{key}"
+        end
+      end
+    end
   end
 end
 
@@ -42,7 +85,8 @@ class ChartBlockProcessor < Asciidoctor::Extensions::BlockProcessor
     }
 
     # The chart definition comes from inside the block
-    parsedContent = parse_content reader
+    parsedContent = ChartMetadata.new
+    parsedContent.parse_content reader
 
     image = generate_chart parent, parsedContent
 
@@ -53,19 +97,22 @@ class ChartBlockProcessor < Asciidoctor::Extensions::BlockProcessor
   def generate_chart(parent, metadata)
     case metadata.type
     when "line"
-      return generate_line_chart parent, metadata
+      g = generate_line_chart parent, metadata
     when "bar"
-      return generate_bar_chart parent, metadata
+      g = generate_bar_chart parent, metadata
     else
       raise "Unsupported chart type #{metadata.type}"
     end
+
+    # set common variables of the chart
+    g.title = metadata.title
+    g.theme = build_theme parent
+    return g
   end
 
   def generate_line_chart(parent, metadata)
     g = Gruff::Line.new
 
-    g.title = metadata.title
-    g.theme = build_theme parent
     g.labels = metadata.data.map { |r| r[metadata.xFields[0]] }
     series = get_series_data metadata
 
@@ -78,8 +125,6 @@ class ChartBlockProcessor < Asciidoctor::Extensions::BlockProcessor
   def generate_bar_chart(parent, metadata)
     g = Gruff::Bar.new
 
-    g.title = metadata.title
-    g.theme = build_theme parent
     g.labels = metadata.data.map { |r| r[metadata.xFields[0]] }
     series = get_series_data metadata
 
@@ -89,52 +134,29 @@ class ChartBlockProcessor < Asciidoctor::Extensions::BlockProcessor
     return g
   end
 
-  def parse_content(reader)
-    metadata = ChartMetadata.new
-    in_data_mode = false
-    first_row = true
-    header = []
-    reader.lines.each do |line|
-      if in_data_mode
-        row_data = line.strip.split(",").map(&:strip)
-        if first_row
-          header = row_data.clone
-          first_row = false
-        else
-          metadata.data << header.zip(row_data).to_h
-        end
-      else
-        setting = line.strip.split(":")
-        case setting[0]
-        when "data"
-          in_data_mode = true
-        when "type"
-          metadata.type = setting[1].strip
-        when "title"
-          metadata.title = setting[1].strip
-        when "x-fields"
-          metadata.xFields = setting[1].strip
-        when "y-fields"
-          metadata.yFields = setting[1].strip.split(',').map(&:strip)
-        else
-          raise "Unknown chart setting #{setting[0]}"
-        end
-      end
-    end
-    return metadata
-  end
-
+  # Generate the theme for use in the chart loading it from the theme to use
+  # in the document
+  #
+  # @param document the document currently being generated
+  # @return the Gruff theme
   def build_theme(parent)
     colors = parent.document.attr("theme-chart-colors",nil)
     return {
-      width: 2200,
+      colors: [ "limegreen", "blue", "purple" ],
+      width: CHART_SCALABLE_WIDTH,
       background_colors: nil
     }
   end
 
+
+  # Builds the series information to send to a chart
+  # 
+  # @param metadata [ChartMetadata]
+  #     The information of the chart after parsing the block
+  # @return The series information
   def get_series_data(metadata)
     series = []
-    metadata.yFields.each do |name|
+    metadata.yFields.each_with_index do |name,index|
       series << {
         name: name,
         values: metadata.data.map { |r| r[name].to_f }
