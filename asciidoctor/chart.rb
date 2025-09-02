24835e1b6da9d72b53ec1f055379744cb1ea0680
Luis Passos <lpcarvalho2@gmail.com>
2025-09-02 11:01:40 +0100

    Include chart configuration to the plugin

--- asciidoctor/chart.rb
+++ asciidoctor/chart.rb
@@ -7,6 +7,18 @@ Asciidoctor::Extensions.register do
   block ChartBlockProcessor, :chart
 end
 
+class ChartMetadata
+  attr_accessor :type, :title, :xFields, :yFields, :yNames, :data
+
+  def initialize()
+    @type = "line"
+    @xFields = []
+    @yFields = []
+    @yNames = []
+    @data = []
+  end
+end
+
 class ChartBlockProcessor < Asciidoctor::Extensions::BlockProcessor
   use_dsl
 
@@ -19,32 +31,90 @@ class ChartBlockProcessor < Asciidoctor::Extensions::BlockProcessor
     image_gen_dir = parent.document.attr('imagesoutdir', '.')
     FileUtils.mkdir_p(image_gen_dir) unless Dir.exist?(image_gen_dir)
 
+    # Auxiliary information specific to the document comes from the block options
     target_image = attrs[:target] || 'default.png'
-    format = attrs[:format] || 'line'
     img_attrs = {
       "alt" => attrs[:caption],
       "width" => "300",
       "target" => image_gen_dir + '/' + target_image
     }
-    image = generate_line_chart("example", {0 => "a", 1 => "b", 2 => "c"}, [
-      {:name => "series1", :values => [5, 10, 12]},
-      {:name => "series2", :values => [8, 5, 6]},
-    ])
+
+    # The chart definition comes from inside the block
+    parsedContent = parse_content reader
+
+    image = generate_chart parent, parsedContent
+
     image.write(image_gen_dir + "/" + target_image)
     create_image_block parent, img_attrs
   end
 
-  def generate_line_chart(title, x, series)
+  def generate_chart(parent, metadata)
+    case metadata.type
+    when "line"
+      return generate_line_chart parent, metadata
+    else
+      raise "Unsupported chart type #{metadata.type}"
+    end
+  end
+
+  def generate_line_chart(parent, metadata)
     g = Gruff::Line.new
 
-    g.title = title
-    g.labels = x
-    g.theme = {
-      background_colors: nil
-    }
+    g.title = metadata.title
+    g.theme = build_theme parent
+    g.labels = metadata.data.map { |r| r[metadata.xFields[0]] }
+    series = []
+    metadata.yFields.each do |name|
+      series << {
+        name: name,
+        values: metadata.data.map { |r| r[name].to_f }
+      }
+    end
     series.each do |y|
       g.data y[:name], y[:values]
     end
     return g
+  end
+
+  def parse_content(reader)
+    metadata = ChartMetadata.new
+    in_data_mode = false
+    first_row = true
+    header = []
+    reader.lines.each do |line|
+      if in_data_mode
+        row_data = line.strip.split(",").map(&:strip)
+        if first_row
+          header = row_data.clone
+          first_row = false
+        else
+          metadata.data << header.zip(row_data).to_h
+        end
+      else
+        setting = line.strip.split(":")
+        case setting[0]
+        when "data"
+          in_data_mode = true
+        when "type"
+          metadata.type = setting[1].strip
+        when "title"
+          metadata.title = setting[1].strip
+        when "x-fields"
+          metadata.xFields = setting[1].strip
+        when "y-fields"
+          metadata.yFields = setting[1].strip.split(',').map(&:strip)
+        else
+          raise "Unknown chart setting #{setting[0]}"
+        end
+      end
+    end
+    return metadata
+  end
+
+  def build_theme(parent)
+    colors = parent.document.attr("theme-chart-colors",nil)
+    return {
+      background_colors: nil
+    }
   end
 end
